8a5cd3cae5a1334b6ce5ebd40ef790edc69dec4b
dotaplayer1 <georgemao72@gmail.com>
2023-01-20 22:42:11 -0800

    Fix 04_removeFromArray

--- html_css_javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ html_css_javascript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    tempArr = arguments[0];
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < tempArr.length; i++) {
+            if (tempArr[i] === arguments[j]) {
+                tempArr.splice(i, 1);
+            }
+        }
+    }
+    return tempArr;
 };
 
 // Do not edit below this line
