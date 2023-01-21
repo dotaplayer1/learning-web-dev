3c9eec0b7d1489622bfb399c67bad8b526c0294a
dotaplayer1 <georgemao72@gmail.com>
2023-01-20 22:58:31 -0800

    Fix 05_sumAll

--- html_css_javascript/javascript-exercises/05_sumAll/sumAll.js
+++ html_css_javascript/javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || num1 < 0) {
+        return "ERROR";
+    }
+    if (typeof num2 != "number" || num2 < 0) {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        let tempNum = num1;
+        for (let i = num1; i <= num2; i++) {
+            tempNum += i;
+        }
+        return tempNum - 1;
+    } else {
+        let tempNum = num2;
+        for (let i = num2; i <= num1; i++) {
+            tempNum += i;
+        }
+        return tempNum - 1; 
+    }
 };
 
 // Do not edit below this line
