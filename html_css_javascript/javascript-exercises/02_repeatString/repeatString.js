404828dfb699fab4061fe441077b28c2a2552083
dotaplayer1 <georgemao72@gmail.com>
2023-01-20 21:57:10 -0800

    Fix 02_repeatString

--- html_css_javascript/javascript-exercises/02_repeatString/repeatString.js
+++ html_css_javascript/javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(input, count) {
+    tempStr = "";
+    if (count >= 1) { 
+        for (let i = 1; i <= count; i++) {
+            tempStr += input;
+        }
+    } else if (count < 0) {
+        return "ERROR";
+    } else {
+        return "";
+    }
+    return tempStr;
 };
 
 // Do not edit below this line
