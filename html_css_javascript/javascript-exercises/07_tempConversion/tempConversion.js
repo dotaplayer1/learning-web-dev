d1a74ad7127f8a6e769c4d224d5015e7a38ffd29
dotaplayer1 <georgemao72@gmail.com>
2023-01-20 23:14:46 -0800

    Fix 07_tempConversion

--- html_css_javascript/javascript-exercises/07_tempConversion/tempConversion.js
+++ html_css_javascript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * (5.0 / 9.0)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * (9.0 / 5.0) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
