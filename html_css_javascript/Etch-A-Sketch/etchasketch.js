d5f74a1bd8cd03f010c85f5dab86f7dea8b6a202
dotaplayer1 <georgemao72@gmail.com>
2023-01-25 19:34:14 -0800

    Add functionality to color tiles when mouse is hovered over them

--- html_css_javascript/Etch-A-Sketch/etchasketch.js
+++ html_css_javascript/Etch-A-Sketch/etchasketch.js
@@ -2,6 +2,11 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 1024; i++) {
     const div = document.createElement("div");
+    div.classList.add("white_background");
+    div.addEventListener("mouseover", () => {
+        div.classList.remove("white_background");
+        div.classList.add("black_background");
+    });
     container.appendChild(div);
     console.log(i);
 }
