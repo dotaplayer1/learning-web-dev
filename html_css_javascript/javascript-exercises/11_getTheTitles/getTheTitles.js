caa90148383073aad72fe08f1d978c3dba9c6196
dotaplayer1 <georgemao72@gmail.com>
2023-01-30 20:54:44 -0800

    Edit 11_getTheTitles folder

--- html_css_javascript/javascript-exercises/11_getTheTitles/getTheTitles.js
+++ html_css_javascript/javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const bookTitles = arr.map(book => `${book.title}`);
+    return bookTitles;
 };
 
 // Do not edit below this line
