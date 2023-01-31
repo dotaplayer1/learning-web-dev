d5803f4f030d69e8625f972f1cd96a3a10b93830
dotaplayer1 <georgemao72@gmail.com>
2023-01-30 20:38:21 -0800

    Edit 10_fibonacci folder

--- html_css_javascript/javascript-exercises/10_fibonacci/fibonacci.js
+++ html_css_javascript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (typeof input == "string") {
+        input = parseInt(input);
+    }
+    if (input <= 0) {
+        return "OOPS";
+    }
+    let n1 = 0;
+    let n2 = 1;
+    let count = 0;
+    while (count < input) {
+        let nth = n1 + n2; 
+        n1 = n2;
+        n2 = nth;
+        count += 1;
+    }
+    return n1;
 };
 
 // Do not edit below this line
