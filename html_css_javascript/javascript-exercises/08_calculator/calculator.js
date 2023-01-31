05af7fd3ec6032a46972dc186593f1e43e5b46d9
dotaplayer1 <georgemao72@gmail.com>
2023-01-30 19:46:32 -0800

    Edit 08_calculator folder

--- html_css_javascript/javascript-exercises/08_calculator/calculator.js
+++ html_css_javascript/javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  let factorialNumber = 1;
+  while (a >= 1) {
+    factorialNumber *= a;
+    a -= 1;
+  }
+  return factorialNumber;
 };
 
 // Do not edit below this line
