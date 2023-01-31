1cadeed709a3198c2cd2249acc7acf87a4910179
dotaplayer1 <georgemao72@gmail.com>
2023-01-30 20:06:06 -0800

    Edit 09_palindromes folder

--- html_css_javascript/javascript-exercises/09_palindromes/palindromes.js
+++ html_css_javascript/javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let str = input.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "");
+    str = str.toLowerCase();
+    return str === str.split("").reverse().join("") ? true :false;
 };
+
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
