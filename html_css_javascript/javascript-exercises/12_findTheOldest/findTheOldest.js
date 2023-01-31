fbfd56b0cbfd39f17f5e026569eb059f70b5048b
dotaplayer1 <georgemao72@gmail.com>
2023-01-30 21:51:52 -0800

    Edit 12_findTheOldest folder

--- html_css_javascript/javascript-exercises/12_findTheOldest/findTheOldest.js
+++ html_css_javascript/javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = 2023;
+    const ages = arr.map(person => typeof person.yearOfDeath == "undefined" ? currentYear - person.yearOfBirth : person.yearOfDeath - person.yearOfBirth);
+    let oldest = 0;
+    let oldest_arrPos = 0;
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > oldest) {
+            oldest_arrPos = i;
+            oldest = ages[i];
+        }
+    }  
+    return arr[oldest_arrPos];
 };
 
 // Do not edit below this line
