1ff0398923c0424954d9ac83b04119b334d7b52f
dotaplayer1 <georgemao72@gmail.com>
2023-02-09 18:08:08 -0800

    Add functionality to create cards on the html page

--- html_css_javascript/libraryapp/script.js
+++ html_css_javascript/libraryapp/script.js
@@ -3,6 +3,7 @@ const author = document.querySelector('#author');
 const pageCount = document.querySelector('#pagecount');
 const isRead = document.querySelector('#hasread');
 const submitButton = document.querySelector('input[type="button"]');
+const displaySection = document.querySelector('#displaysection');
 
 let myLibrary = [];
 
@@ -13,8 +14,54 @@ function Book(title, author, pageCount, isRead) {
     this.isRead = isRead;
 }
 
-function displayLibrary() {
-    for (let i = 0; )
+Book.prototype.cardFactory = function() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const cardTitle = document.createElement('p');
+    cardTitle.textContent = `Title = ${this.title}`;
+    card.appendChild(cardTitle);
+
+    const cardAuthor = document.createElement('p');
+    cardAuthor.textContent = `Author = ${this.author}`;
+    card.appendChild(cardAuthor);
+
+    const cardPageCount = document.createElement('p');
+    cardPageCount.textContent = `PageCount = ${this.pageCount}`;
+    card.appendChild(cardPageCount);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('deletebutton');
+    deleteButton.textContent = "Delete Item";
+    card.appendChild(deleteButton);
+    deleteButton.onclick = () => card.remove();
+    
+    const hasReadButton = document.createElement('button');
+    if (this.isRead = true) {
+        hasReadButton.classList.add('is_read');
+        hasReadButton.textContent = 'Read';
+    } else {
+        hasReadButton.classList.add('is_not_read');
+        hasReadButton.textContent = 'Not Read';
+    }
+    card.appendChild(hasReadButton);
+    hasReadButton.onclick = () => {
+        if (hasReadButton.classList.contains('is_read')) {
+            hasReadButton.classList.remove('is_read');
+            hasReadButton.classList.add('is_not_read');
+            hasReadButton.textContent = 'Not Read';
+        } else {
+            hasReadButton.classList.add('is_read');
+            hasReadButton.classList.remove('is_not_read');
+            hasReadButton.textContent = 'Read';
+        }
+    };
+    
+    displaySection.appendChild(card);
+}
+
+function displayLibrary(bookToDisplay) {
+    bookToDisplay.cardFactory();
 }
 
 function addBookToLibrary() {
@@ -27,7 +74,7 @@ function addBookToLibrary() {
     console.log(newBook);
     myLibrary.push(newBook); 
 
-    displayLibrary();
+    displayLibrary(newBook);
 }
 
 submitButton.addEventListener("click", addBookToLibrary);
