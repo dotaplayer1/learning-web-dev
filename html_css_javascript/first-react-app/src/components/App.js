448f08adfe891ccc4ae82d61444c94c6cbafe623
dotaplayer1 <georgemao72@gmail.com>
2023-03-07 13:33:27 -0800

    Create first react app

---
+++ html_css_javascript/first-react-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <div className='fasdf'>Testing testing 123</div>
+        );
+    }
+}
