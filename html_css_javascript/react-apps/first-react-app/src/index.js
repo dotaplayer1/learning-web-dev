8f2de3a1c4085bf1b93c1b9c22c0fc31ecada3e9
dotaplayer1 <georgemao72@gmail.com>
2023-03-08 20:44:31 -0800

    Play around with react in first-react-app

--- html_css_javascript/react-apps/first-react-app/src/index.js
+++ html_css_javascript/react-apps/first-react-app/src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './components/App';
-import { Header } from './components/Header';
-import { Section } from './components/Section';
-import { Footer } from './components/Footer';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-        <Header />
-        <Section />
-        <Footer />
-    </React.StrictMode>
+
+function formatName(user) {
+    return user.firstName + " " + user.lastName;
+}
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    } else {
+        return <h1>Hello, Stranger.</h1>
+    }
+}
+
+const user = {
+    firstName: "John",
+    lastName: "Smith"
+};
+
+const element = <a href="https://www.reactjs.org"> link </a>;
+
+const parentDivWithChildren = (
+    <div>
+        <h1>Hello!</h1>
+        <h2>Good to see you here.</h2>
+    </div>
 );
+
+function tick() {
+    const timeElement = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    root.render(timeElement);
+}
+
+setInterval(tick, 1000);
