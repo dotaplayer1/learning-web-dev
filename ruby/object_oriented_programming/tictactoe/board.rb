59946ce40bc1de853eb630e9012a328516a8270c
dotaplayer1 <georgemao72@gmail.com>
2023-03-26 04:07:24 -0700

    Add valid_move? and place_marker methods to Board class

--- ruby/object_oriented_programming/tictactoe/board.rb
+++ ruby/object_oriented_programming/tictactoe/board.rb
@@ -1,3 +1,4 @@
+# a class for a tictactoe board
 class Board
   attr_reader :board
 
@@ -8,11 +9,11 @@ class Board
   WINNING_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
   def print_board
-     puts " #{board[1]} | #{board[2]} | #{board[3]} "
-     puts "---+---+---"
-     puts " #{board[4]} | #{board[5]} | #{board[6]} "
-     puts "---+---+---"
-     puts " #{board[7]} | #{board[8]} | #{board[9]} "
+    puts " #{board[1]} | #{board[2]} | #{board[3]} "
+    puts "---+---+---"
+    puts " #{board[4]} | #{board[5]} | #{board[6]} "
+    puts "---+---+---"
+    puts " #{board[7]} | #{board[8]} | #{board[9]} "
   end
 
   def board_full?
@@ -21,11 +22,19 @@ class Board
 
   def game_is_won?
     WINNING_POSITIONS.each do |winning_pos|
-      if [board[winning_pos[0]], 
-          board[winning_pos[1]], 
+      if [board[winning_pos[0]],
+          board[winning_pos[1]],
           board[winning_pos[2]]].uniq.length == 1
         return true
       end
     end
+  end
+
+  def valid_move?(input)
+    board[input] == input
+  end
+
+  def place_marker(input, marker)
+    board[input] == marker
   end
 end
