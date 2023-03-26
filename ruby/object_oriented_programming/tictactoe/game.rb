1d08b9374b14e95075304f8913ca378e58ae3d22
dotaplayer1 <georgemao72@gmail.com>
2023-03-26 05:12:57 -0700

    Create play_a_turn method and update switch_turns

--- ruby/object_oriented_programming/tictactoe/game.rb
+++ ruby/object_oriented_programming/tictactoe/game.rb
@@ -4,9 +4,6 @@ require_relative "board.rb"
 class Tictactoe
   attr_reader :board
 
-  PLAYER_1_MARKER = "X"
-  PLAYER_2_MARKER = "O"
-
   def initialize
     @board = Board.new
     @current_player = 1
@@ -15,11 +12,13 @@ class Tictactoe
   def play
     while true
       board.print_board
-      players_turn
+      play_a_turn
       if board.game_is_won?
+        board.print_board
         puts "Player #{@current_player} wins!"
         return
       elsif board.board_full?
+        board.print_board
         puts "Tie game..."
         return
       else
@@ -29,10 +28,25 @@ class Tictactoe
   end
 
   def switch_turns
-    current_player == 1 ? current_player = 2 : current_player = 1
+    @current_player == 1 ? @current_player = 2 : @current_player = 1
   end
 
-  def players_turn
-    
+  def play_a_turn
+    input = 0
+    while true
+      print "Player #{@current_player}, Enter a position: "
+      input = gets.chomp.to_i
+      if board.valid_move?(input)
+        case @current_player
+        when 1
+          board.place_marker(input, "X")
+        when 2
+          board.place_marker(input, "O")
+        end
+        return
+      else
+        puts "Invalid move"
+      end
+    end
   end
 end
