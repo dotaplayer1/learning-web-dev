b45d20f57c75ab5e4cfe77bb3130582886339f5f
dotaplayer1 <georgemao72@gmail.com>
2023-03-27 04:31:34 -0700

    Finish play method

--- ruby/object_oriented_programming/mastermind/game.rb
+++ ruby/object_oriented_programming/mastermind/game.rb
@@ -2,8 +2,7 @@ require_relative "game_logic.rb"
 
 # Contains the game logic for the mastermind game
 class Game
-  attr_reader :game_logic
-  attr_accessor :turns
+  attr_reader :game_logic, :turns
 
   def initialize
     @game_logic = GameLogic.new
@@ -13,9 +12,20 @@ class Game
   def play
     puts "A code has been generated. Good luck guessing it :)"
     while turns > 0
+      puts "Number of turns left: #{turns}"
       input = ask_for_input
-      puts input
+      if game_logic.code_is_correct?(input)
+        puts "The code is correct! You win!"
+        return
+      else
+        results = game_logic.compare_with_code(input) 
+        puts "Correct numbers in correct positions: #{results[0]}"
+        puts "Correct numbers in incorrect positions: #{results[1]}"
+      end
+      puts ""
+      @turns -= 1
     end
+    puts "You lose! The correct code was #{game_logic.random_code.join}"
   end
 
   def ask_for_input
@@ -23,7 +33,7 @@ class Game
       print "Enter a 4 digit guess containing numbers between 1-6: "
       input = gets.chomp
       if is_valid_input?(input)
-        mutate_input(input)
+        input = mutate_input(input)
         return input
       else
         puts "Invalid input."
