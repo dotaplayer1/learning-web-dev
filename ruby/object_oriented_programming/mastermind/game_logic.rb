869e34c72fe65faac58eff3209d68ea78bef2e6a
dotaplayer1 <georgemao72@gmail.com>
2023-03-27 04:31:08 -0700

    Add code validation methods

--- ruby/object_oriented_programming/mastermind/game_logic.rb
+++ ruby/object_oriented_programming/mastermind/game_logic.rb
@@ -3,13 +3,42 @@ class GameLogic
   attr_reader :random_code
 
   def initialize
-    @random_code = generate_code
+    @random_code = [3, 3, 3, 4]
   end
 
   def generate_code
     [rand(1..6), rand(1..6), rand(1..6), rand(1..6)]
   end
-end
 
-board = GameLogic.new
-puts board.random_code
+  def code_is_correct?(input)
+    input == random_code
+  end
+
+  def compare_with_code(input)
+    [exact_matches(input), non_exact_matches(input)]
+  end
+
+  def exact_matches(input)
+    count = 0
+    input.each_with_index do |value, index|
+      if value == random_code[index]
+        count += 1
+      end
+    end
+    count
+  end
+
+  def non_exact_matches(input)
+    count = 0
+    cloned_code = random_code.clone
+    input.each_with_index do |value, index|
+      if value != cloned_code[index]
+        if cloned_code.include?(value)
+          count += 1
+          cloned_code[cloned_code.index(value)] = nil
+        end
+      end
+    end
+    count
+  end
+end
