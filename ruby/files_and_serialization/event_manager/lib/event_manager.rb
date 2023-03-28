122e2baedce2bf8a5dae0fe7e52fc86947e0f316
dotaplayer1 <georgemao72@gmail.com>
2023-03-28 14:17:51 -0700

    Add print_numbers and clean_number methods

--- ruby/files_and_serialization/event_manager/lib/event_manager.rb
+++ ruby/files_and_serialization/event_manager/lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'pry'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -33,22 +34,56 @@ end
 
 puts 'EventManager initialized.'
 
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
+# contents = CSV.open(
+#   'event_attendees.csv',
+#   headers: true,
+#   header_converters: :symbol
+# )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+# template_letter = File.read('form_letter.erb')
+# erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
-  legislators = legislators_by_zipcode(zipcode)
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
 
-  form_letter = erb_template.result(binding)
+#   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+#   save_thank_you_letter(id,form_letter)
+# end
+
+def open_file
+  contents = CSV.open(
+    "event_attendees.csv",
+    headers: true,
+    header_converters: :symbol
+  )
 end
+
+def print_numbers
+  contents = open_file
+
+  contents.each do |row|
+    name = row[:first_name]
+    phone_number = clean_number(row[:homephone])
+    puts "#{name} #{phone_number}"
+  end
+end
+
+def clean_number(number)
+  return "?" if number.nil?
+  phone_number = number.gsub(/[^0-9]/, '')
+  binding.pry if phone_number == 14018685000
+  if phone_number.chars[0] == "1" && phone_number.length > 10
+    phone_number = phone_number.to_s[1..-1]
+  end
+  if phone_number.length == 10
+    phone_number
+  else
+    "Invalid number"
+  end
+end
+
+print_numbers
