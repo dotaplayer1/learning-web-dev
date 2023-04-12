385bc4782cd5ffacb6a8f3c3d0b89efac8849c10
dotaplayer1 <georgemao72@gmail.com>
2023-04-12 12:02:55 -0700

    I am starting to dislike webpack

--- html_css_javascript/weatherapp/src/index.js
+++ html_css_javascript/weatherapp/src/index.js
@@ -1,1 +1,10 @@
-alert(1);
+import './scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+
+async function getWeatherData(location) {
+  const fetchedData = await fetch(`https://api.weatherapi.com/v1/current.json?key=099a9c0ffda84a2ba86110316231204&q=${location}`)
+  const responseJson = await fetchedData.json();
+  console.log(responseJson);
+}
+
+getWeatherData("vancouver");
