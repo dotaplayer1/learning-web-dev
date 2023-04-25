e924a207a8f848207f769fb4f3794db0978eab77
dotaplayer1 <georgemao72@gmail.com>
2023-04-24 22:39:23 -0700

    Finish game.rb and board.rb

--- ruby/testing_with_rspec/connect_4/lib/board.rb
+++ ruby/testing_with_rspec/connect_4/lib/board.rb
@@ -20,6 +20,7 @@ class Board
       end
       puts ""
     end
+    puts "1 2 3 4 5 6 7"
   end
 
   def print_tile(tile)
--- ruby/testing_with_rspec/connect_4/lib/game.rb
+++ ruby/testing_with_rspec/connect_4/lib/game.rb
@@ -15,22 +15,30 @@ class Game
     loop do
       board.print_board
       valid_move = get_valid_move
+      place_marker(valid_move)
+      return if game_end?(valid_move)
       update_game(valid_move)
-      if game_won?
-        board.print_board
-        puts "Game won!" 
-        return
-      elsif tie_game?(turns_played) 
-        board.print_board
-        puts "Tie game!"
-        return
-      end
       swap_turns
     end
   end
 
-  def update_game(valid_move)
+  def place_marker(valid_move)
     board.place_marker(5 - column_height[valid_move], valid_move, player)
+  end
+
+  def game_end?(valid_move)
+    if tie_game?(turns_played)
+      board.print_board
+      puts "\nTie game!"
+      true
+    elsif game_won?(valid_move)
+      board.print_board
+      puts "\nPlayer #{player} wins!"
+      true
+    end
+  end
+
+  def update_game(valid_move)
     column_height[valid_move] += 1
     @turns_played += 1
   end
@@ -42,6 +50,7 @@ class Game
   def get_valid_move
     input = -1
     loop do
+      puts ""
       puts "Player #{player}, Enter a column: "
       input = ask_for_input
       break unless is_valid_move?(input) == false
@@ -70,7 +79,93 @@ class Game
     turns == 42
   end
 
-  def game_won?
+  def game_won?(move)
+    return true if horizontal_win?(move)
+    return true if vertical_win?(move) 
+    return true if diagonal_win_from_bottom_left?(move)
+    return true if diagonal_win_from_bottom_right?(move)
+  end
+
+  def horizontal_win?(move)
+    row = board.board[5 - column_height[move]]
+    (0..3).each do |i|
+      position = [row[0 + i], row[1 + i], row[2 + i], row[3 + i]]
+      return true if position == [1, 1, 1, 1] || position == [2, 2, 2, 2]
+    end
+    false
+  end
+
+  def vertical_win?(move)
+    column = [
+      board.board[0][move],
+      board.board[1][move],
+      board.board[2][move],
+      board.board[3][move],
+      board.board[4][move],
+      board.board[5][move],
+    ]
+    (0..3).each do |i|
+      position = [column[0 + i], column[1 + i], column[2 + i], column[3 + i]]
+      return true if position == [1, 1, 1, 1] || position == [2, 2, 2, 2]
+    end
+    false
+  end
+
+  def diagonal_win_from_bottom_left?(move)
+    diagonal = []
+    bottom_left_x = -3
+    bottom_left_y = 3
+    7.times do
+      in_bounds = true
+      x_pos = bottom_left_x + move
+      y_pos = bottom_left_y + 5 - column_height[move]
+      if x_pos < 0 || x_pos > 6
+        in_bounds = false
+      end
+      if y_pos < 0 || y_pos > 5
+        in_bounds = false
+      end
+      if in_bounds
+        diagonal.push(board.board[y_pos][x_pos])
+      end
+      bottom_left_x += 1
+      bottom_left_y -= 1
+    end
+    if diagonal.length >= 4
+      (0..diagonal.length - 4).each do |i|
+        position = [diagonal[0 + i], diagonal[1 + i], diagonal[2 + i], diagonal[3 + i]]
+        return true if position == [1, 1, 1, 1] || position == [2, 2, 2, 2]
+      end
+    end
+    false
+  end
+
+  def diagonal_win_from_bottom_right?(move)
+    diagonal = []
+    bottom_left_x = 3
+    bottom_left_y = 3
+    7.times do
+      in_bounds = true
+      x_pos = bottom_left_x + move
+      y_pos = bottom_left_y + 5 - column_height[move]
+      if x_pos < 0 || x_pos > 6
+        in_bounds = false
+      end
+      if y_pos < 0 || y_pos > 5
+        in_bounds = false
+      end
+      if in_bounds
+        diagonal.push(board.board[y_pos][x_pos])
+      end
+      bottom_left_x -= 1
+      bottom_left_y -= 1
+    end
+    if diagonal.length >= 4
+      (0..diagonal.length - 4).each do |i|
+        position = [diagonal[0 + i], diagonal[1 + i], diagonal[2 + i], diagonal[3 + i]]
+        return true if position == [1, 1, 1, 1] || position == [2, 2, 2, 2]
+      end
+    end
     false
   end
 end
